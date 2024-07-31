f7f3f6c2d5d4954ccc3139c7774ba96be8eb59ec
Queen Uwamucyo <queenwamucyo@gmail.com>
2024-07-31 15:52:48 +0200

    task 2

--- task_2.sql
+++ task_2.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS Orders (
 );
 
 CREATE TABLE IF NOT EXISTS Order_details (
-    Orderdetails_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_details_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
