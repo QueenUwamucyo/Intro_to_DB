548f5945e65a96c2756e55c124aab79e7474caa8
Queen Uwamucyo <queenwamucyo@gmail.com>
2024-07-31 15:42:43 +0200

    task 2

--- task_2.sql
+++ task_2.sql
@@ -29,10 +29,11 @@ CREATE TABLE IF NOT EXISTS Orders (
 );
 
 CREATE TABLE IF NOT EXISTS Order_details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES books(book_id) 
 );
 USE alx_book_store;
